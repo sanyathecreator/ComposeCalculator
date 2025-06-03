0ef5eb70b752b1c1f731e103bd3739a7abbaa8d8
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-06-03 18:28:40 +0200

    Add calculator display composable

--- app/src/main/java/com/example/composecalculator/MainActivity.kt
+++ app/src/main/java/com/example/composecalculator/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.composecalculator.ui.theme.ComposeCalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,6 +15,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeCalculatorTheme {
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
 
             }
         }
